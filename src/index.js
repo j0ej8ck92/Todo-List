aeb5cf7becdd959177d00f563ae63b4e42ae7367
j0ej8ck92 <j0ej8ck92@gmail.com>
2026-06-19 01:51:22 +0000

    Keepin' it Classy and keepin' it Single!

--- src/index.js
+++ src/index.js
@@ -1,38 +1,52 @@
 import "./styles.css";
-import {greeting} from "./module-1.js";
+import {greeting, Task, addTaskToArray, arrOfTasks} from "./module-1.js";
 
 console.log(greeting);
 
-const arrOfTasks = [];
 
-class Task {
-    constructor(title, description, notes, dueDate, priority){
-        this.title = title;
-        this.description = description;
-        this.notes = notes;
-        this.dueDate = dueDate;
-        this.priority = priority;
-    }
-}
+const firstTask = new Task("run", "three miles", "under 25 mins", "June 4, 2026", "medium");
 
-const addTaskToArray = function(title, description, notes, dueDate, priority){
-    const newTask = new Task(title, description, notes, dueDate, priority);
-    console.log(arrOfTasks)
-    return arrOfTasks.push(newTask);
-}
+const secondTask = new Task("jog", "four miles", "under 50 mins", "June 18, 2026", "high");
 
-const firstTask = addTaskToArray("run", "three miles", "under 25 mins", "June 4, 2026", "medium");
 
-const secondTask = addTaskToArray("jog", "four miles", "under 50 mins", "June 18, 2026", "high");
-
-console.log(firstTask);
-console.log(secondTask);
-
-console.log(arrOfTasks);
-
-const taskButton = document.querySelector(".add-task");
+/*const taskButton = document.querySelector(".add-task");
 
 console.log(taskButton);
 
-taskButton.addEventListener("click", () => addTaskToArray());
+taskButton.addEventListener("click", () => addTaskToArray());*/
+
+
+console.log(firstTask);
+
+//debugger;
+console.log(firstTask.sayPriority());
+
+addTaskToArray(firstTask);
+addTaskToArray(secondTask);
+
+console.log(arrOfTasks);
+
+const form = document.getElementById("form");
+const taskTitle = document.getElementById("task-title");
+const taskDescription = document.getElementById("task-description");
+const taskNotes = document.getElementById("task-notes");
+const taskDueDate = document.getElementById("task-due-date");
+const taskPriority = document.getElementById("task-priority");
+const submitButton = document.getElementById("submit");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log(taskTitle.value)
+    const newTask = new Task(taskTitle.value, taskDescription.value, taskNotes.value, taskDueDate.value, taskPriority.value);
+    console.log(newTask);
+    addTaskToArray(newTask);
+   console.log(arrOfTasks);
+})
+
+
+const getArr = arrOfTasks[1].sayPriority(); //useful for using methods within task Array of task class objects!
+                                      
+console.log(getArr);
+
+console.log(arrOfTasks[1].notes);
 
--- src/module-1.js
+++ src/module-1.js
@@ -1,1 +1,28 @@
 export const greeting = "Hello Odinite!";
+
+
+export class Task {
+    constructor(title, description, notes, dueDate, priority){
+        this.title = title;
+        this.description = description;
+        this.notes = notes;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+}
+
+//const taskMethodPriority = {
+
+    Task.prototype.sayPriority = function(){
+        const saying = `This title has a priority of ${this.priority}.`
+        return saying;
+    }
+//}
+
+//Object.assign(Task.prototype, taskMethodPriority); 
+
+export const addTaskToArray = function(task){
+    return arrOfTasks.push(task);
+}
+
+export const arrOfTasks = [];
